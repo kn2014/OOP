97c86a0c2290e77f3ead8c383c19f2ff5fe79ee9
kn2014 <cs1.8@mail.bg>
2015-03-12 15:52:20 +0200

    Create Product.cpp

---
+++ Exercises/12.03/Product.cpp
@@ -0,0 +1,61 @@
+#include "Product.h"
+
+Product::Product(char* n,int q, double p) : name(NULL)
+{
+    if(n!=NULL)
+    {
+        SetName(n);
+        SetPrice(p);
+        SetQuantity(q);
+
+    }
+}
+
+Product::Product(const Product& other) : name(NULL)
+{
+    if(other.name!=NULL)
+    {
+        SetName(other.name);
+        SetPrice(other.price);
+        SetQuantity(other.quantity);
+    }
+}
+
+const char* Product::GetName() const
+{
+    return name;
+}
+
+int Product::GetQuantity() const
+{
+    return quantity;
+}
+
+double Product::GetPrice() const
+{
+    return price;
+}
+
+void Product::SetName(char* n)
+{
+    if(name!=NULL) { delete [] name; }
+    name = new char[strlen(n)+1];
+    assert(name!=NULL);
+    strcpy(name,n);
+}
+
+void Product::SetQuantity(int q)
+{
+    if(q>=0)
+    {
+        quantity = q;
+    }
+}
+
+void Product::SetPrice(double p)
+{
+    if(p>=0)
+    {
+        price = p;
+    }
+}
