345d86938b091b37cac433a48fced40a182f953c
kn2014 <cs1.8@mail.bg>
2015-03-19 19:03:11 +0200

    Update Product.h

--- Exercises/12.03/Product.h
+++ Exercises/12.03/Product.h
@@ -1,31 +1,27 @@
 #ifndef PRODUCT_H
 #define PRODUCT_H
-
 #include <iostream>
 #include <string.h>
 #include <cassert>
-
 using namespace std;
-
 class Product
 {
-    private :
-        char* name;
-        int quantity;
-        double price;
-    public :
-        Product() { name = NULL; quantity=0; price=0; }
-        ~Product() { if(name!=NULL) { delete [] name; } }
-        Product(char*,int,double);
-        Product(const Product&);
-        double GetPrice() const;
-        int GetQuantity() const;
-        const char* GetName() const;
-        void SetName(char*);
-        void SetQuantity(int);
-        void SetPrice(double);
-        Product& operator= (const Product& other);
+private :
+    char* name;
+    int quantity;
+    double price;
+public :
+    Product();
+    ~Product();
+    Product(char*,int,double);
+    Product(const Product&);
+    double getPrice() const;
+    int getQuantity() const;
+    const char* getName() const;
+    void SetName(char*);
+    void SetQuantity(int);
+    void SetPrice(double);
+    Product& operator= (const Product& other);
+
 };
-
-
 #endif // PRODUCT_H
