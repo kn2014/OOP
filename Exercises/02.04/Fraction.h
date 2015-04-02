2a9ac7a41aff77bcd2c9a1fe541eaa5f831b5fa7
kn2014 <cs1.8@mail.bg>
2015-04-02 16:38:59 +0300

    Update Fraction.h

--- Exercises/02.04/Fraction.h
+++ Exercises/02.04/Fraction.h
@@ -3,12 +3,12 @@
 
 class Fraction
 {
-    private :
+    private:
         int     num,den;
         void    skr();
 
     public:
-        Fraction(int,int);
+        Fraction(int = 0,int = 1);
         ~Fraction() {};
         int     getNum();
         int     getDen();
@@ -16,12 +16,14 @@ class Fraction
         void    setDen(int);
         void print() const;
         Fraction operator+(const Fraction&)const;
+        Fraction operator+(const int&)const;
         Fraction operator-(const Fraction&)const;
         Fraction operator*(const Fraction&)const;
         Fraction operator/(const Fraction&)const;
         bool operator==(const Fraction&)const;
         bool operator>(const Fraction&)const;
         bool operator<(const Fraction&)const;
+        friend Fraction operator+(const int&,const Fraction&);
 
 };
 
