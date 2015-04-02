e34c8ba65bea749d8fa72270cd01fccf8102b46f
kn2014 <cs1.8@mail.bg>
2015-04-02 16:24:10 +0300

    Create Fraction.cpp

---
+++ Exercises/02.04/Fraction.cpp
@@ -0,0 +1,107 @@
+#include "Fraction.h"
+#include<math.h>
+#include<iostream>
+
+
+int gcd(int a, int b)
+{
+    a = fabs(a);
+    b = fabs(b);
+  if (b == 0)
+     return a;
+  else
+     return gcd(b, a%b);
+}
+
+int nok (int a, int b){
+    return (a*b)/gcd(a, b);
+}
+void Fraction::skr()
+{
+    int k = gcd(num,den);
+    num = num/k;
+    den = den/k;
+}
+
+Fraction::Fraction(int n=0,int d=1 )
+{
+    num = n;
+    den = d;
+    skr();
+}
+
+void Fraction::setDen(int d)
+{
+    if(d<=0) {  }
+    den=d;
+    skr();
+}
+
+void Fraction::setNum(int n)
+{
+    num=n;
+    skr();
+}
+
+int Fraction::getNum()
+{
+    return num;
+}
+
+int Fraction::getDen()
+{
+    return den;
+}
+
+void Fraction::print()const{
+    std::cout<<num<<"/"<<den<<std::endl;
+}
+
+Fraction Fraction::operator+(const Fraction& other) const
+{
+    Fraction result;
+    result.den = nok(den, other.den);
+    int n1 = result.den/den, n2 = result.den/other.den;
+    result.num = num*n1 + other.num*n2;
+    result.skr();
+    return result;
+}
+
+Fraction Fraction::operator-(const Fraction& other) const
+{
+    Fraction result;
+    result.den = nok(den, other.den);
+    int n1 = result.den/den, n2 = result.den/other.den;
+    result.num = num*n1 - other.num*n2;
+    result.skr();
+    return result;
+}
+
+Fraction Fraction::operator*(const Fraction& other) const{
+    Fraction result;
+    result.num = num * other.num;
+    result.den = den * other.den;
+    result.skr();
+    return result;
+}
+
+Fraction Fraction::operator/(const Fraction& other) const{
+    Fraction result;
+    result.num = num * other.den;
+    result.den = den * other.num;
+    result.skr();
+    return result;
+}
+
+bool Fraction::operator==(const Fraction& other) const{
+    return num * other.den == den * other.num;
+}
+
+bool Fraction::operator<(const Fraction& other) const{
+    return num * other.den < den * other.num;
+}
+
+bool Fraction::operator>(const Fraction& other) const{
+    return !(*this<other);
+}
+
