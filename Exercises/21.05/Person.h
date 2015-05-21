8059d068168b2223ad03d4ac3d2b73922ac4b5aa
kn2014 <cs1.8@mail.bg>
2015-05-21 16:44:52 +0300

    Create Person.h

---
+++ Exercises/21.05/Person.h
@@ -0,0 +1,32 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string.h>
+#include <iostream>
+
+class Person
+{
+    public:
+        Person();
+        Person(const char*,bool,int);
+        Person(const Person&);
+        Person& operator=(const Person&);
+        int GetAge() const;
+        bool GetGender() const;
+        const char* GetName() const;
+        void SetAge(int);
+        void SetName(const char*);
+        void SetGender(bool);
+
+        void Print() const;
+
+        virtual ~Person();
+
+    protected:
+        int age;
+    private:
+        char* name;
+        bool gender;
+};
+
+#endif // PERSON_H
