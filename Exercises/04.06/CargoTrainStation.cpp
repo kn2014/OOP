f1eed7e2b77826064792f9a106f23c1855ea60e5
kn2014 <cs1.8@mail.bg>
2015-06-04 17:07:37 +0300

    Create CargoTrainStation.cpp

---
+++ Exercises/04.06/CargoTrainStation.cpp
@@ -0,0 +1,33 @@
+#include "TrainStation.cpp"
+
+#pragma once
+
+class CargoTrainStation : virtual public TrainStation{
+    float load[150];
+
+public:
+
+    CargoTrainStation(const char* loc) : TrainStation(loc){
+        for(int i = 0; i<150; i++){
+            load[i] = 0;
+        }
+    }
+
+    void setLoadAt(unsigned where, float what){
+        load[where] = what;
+    }
+
+    float getLoadAt(unsigned where){
+        return load[where];
+    }
+
+    bool isCargo()const {
+        return true;
+    }
+
+    bool isPeople()const{
+        return false;
+    }
+
+};
+
